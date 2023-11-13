eee49527ff9de532f5eaf3acf860d946910e48c1
akbar <akbarfaisal17@gmail.com>
2023-11-13 23:28:54 +0700

    lenovo

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import { data } from "autoprefixer";
-import FilmList from "./search/components/FilmList";
-
-const Home = async () => {
-
-  const response = await fetch(
-    `https://api.jikan.moe/v4/top/anime`
-  );
-
-  const anime = await response.json();
- 
-
-  return (
-    <>
-      <h1>Daftar Populer</h1>
-
-      {anime.data.map(data => {
-        return <FilmList title={data.title} images={data.images.webp.image_url}/>
-        
-      })}
-      
-    </>  
-  )
-
-=======
 import FilmList from "@/components/FilmList";
 import Link from "next/link";
 
@@ -60,6 +34,5 @@ const Home = async () => {
       </div>
     </>
   );
->>>>>>> 35a076a97dba18968b00963ba025be6d7e90b0aa
 };
 export default Home;
