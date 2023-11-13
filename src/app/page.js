ab990f54d4fcf8bad0342b9d5fa6fb3249b2f493
akbar <akbarfaisal17@gmail.com>
2023-11-14 00:03:20 +0700

    Revert "Merge pull request #2 from Grymore/master"
    This reverts commit 6b0d07fe4728f395539275abb2abd4781e5d498f, reversing
    changes made to f50d04701e502ef5a74a8c4611369d447b3aaf20.

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
-
+<<<<<<< HEAD
 import { data } from "autoprefixer";
 import FilmList from "./search/components/FilmList";
 
 const Home = async () => {
-  const response = await fetch(`https://api.jikan.moe/v4/top/anime`);
+
+  const response = await fetch(
+    `https://api.jikan.moe/v4/top/anime`
+  );
 
   const anime = await response.json();
+ 
 
   return (
     <>
@@ -33,5 +37,6 @@ const Home = async () => {
       </div>
     </>
   );
+>>>>>>> 35a076a97dba18968b00963ba025be6d7e90b0aa
 };
 export default Home;
--- src/app/search/components/Search/index.js
+++ src/app/search/components/Search/index.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 const Search = () => {
   return <>Ini Search component</>;
 };
 
 export default Search;
-=======
-const Search = () => {
-  return <>Ini Search component</>;
-};
-
-export default Search;
->>>>>>> parent of b623626 (session 1)
