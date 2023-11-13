32e858c96216ec21eb46d85c0a6fe6cddc45c7ae
faisal@doku.com <faisal@doku.com>
2023-11-13 10:49:19 +0700

    update docker

--- src/app/page.js
+++ src/app/page.js
@@ -2,25 +2,20 @@ import { data } from "autoprefixer";
 import FilmList from "./search/components/FilmList";
 
 const Home = async () => {
-
-  const response = await fetch(
-    `https://api.jikan.moe/v4/top/anime`
-  );
+  const response = await fetch(`https://api.jikan.moe/v4/top/anime`);
 
   const anime = await response.json();
- 
 
   return (
     <>
       <h1>Daftar Populer</h1>
 
-      {anime.data.map(data => {
-        return <FilmList title={data.title} images={data.images.webp.image_url}/>
-        
+      {anime.data.map((data) => {
+        return (
+          <FilmList title={data.title} images={data.images.webp.image_url} />
+        );
       })}
-      
-    </>  
-  )
-
+    </>
+  );
 };
 export default Home;
